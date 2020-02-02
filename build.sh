d32ecda350fc070e51c38d11c4b1cd889bbc6e0a
prsdsrm <60560623+prsdsrm@users.noreply.github.com>
2020-02-02 17:25:35 -0600

    Add initial Jenkinsfile

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ wget https://www.openssl.org/source/openssl-"$OPENSSL_VER".tar.gz
 
 tar xzf openssl-"$OPENSSL_VER".tar.gz
 
-cd $PKG_HOME/*$OpenSSL_VERSION*
+cd *$OpenSSL_VERSION*
 ./config --prefix=$DEST_HOME/openssl --openssldir=$DEST_HOME/openssl shared zlib
 make && make install
